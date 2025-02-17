a9f521e6147031157ebee544b1f09cc754ed5f5f
Kunwoo Park <105582355+gunnuuu@users.noreply.github.com>
2025-02-17 13:04:24 +0900

    add  findBusInfoByStationNumber

--- src/main/java/project/paypass/repository/stationRepository.java
+++ src/main/java/project/paypass/repository/stationRepository.java
@@ -1,7 +1,12 @@
 package project.paypass.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import project.paypass.domain.Station;
 
 public interface StationRepository extends JpaRepository<Station, Long> {
+
+    @Query("select s.busInfo from Station s where s.stationNumber = :stationNumber")
+    String findBusInfoByStationNumber(@Param("stationNumber")Long stationNumber);
 }
