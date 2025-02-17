a6144c5394a0110a87f14be3d1868e5dda22230a
Kunwoo Park <105582355+gunnuuu@users.noreply.github.com>
2025-02-17 13:56:04 +0900

    add findBusInfoByStationNumber method

--- src/main/java/project/paypass/service/StationService.java
+++ src/main/java/project/paypass/service/StationService.java
@@ -1,5 +1,6 @@
 package project.paypass.service;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import project.paypass.domain.Station;
@@ -13,8 +14,14 @@ public class StationService {
 
     private final StationRepository stationRepository;
 
+    @Transactional
     public List<Station> findAll(){
         return stationRepository.findAll();
+    }
+
+    @Transactional
+    public String findBusInfoByStationNumber(Long stationNumber){
+        return stationRepository.findBusInfoByStationNumber(stationNumber);
     }
 
 }
