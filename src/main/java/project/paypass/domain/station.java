deb3ca38ad0955904f38a70cfc89c762a1947036
Kunwoo Park <105582355+gunnuuu@users.noreply.github.com>
2025-02-17 12:52:49 +0900

    bustInfo text타입으로 번경

--- src/main/java/project/paypass/domain/station.java
+++ src/main/java/project/paypass/domain/station.java
@@ -1,9 +1,6 @@
 package project.paypass.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -22,4 +19,7 @@ public class Station {
     private double longitude;
 
     private double latitude;
+
+    @Column(columnDefinition = "TEXT")
+    private String busInfo;
 }
