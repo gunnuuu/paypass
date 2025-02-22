225737c03f2ec52e37e85007cc5d345a90266018
Kunwoo Park <105582355+gunnuuu@users.noreply.github.com>
2025-02-22 17:12:18 +0900

    버스정류장별 이동시간 담긴 BusTime 테이블 생성

---
+++ src/main/java/project/paypass/domain/BusTime.java
@@ -0,0 +1,29 @@
+package project.paypass.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Entity
+@Data
+public class BusTime {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String routeId;
+
+    @Column(nullable = false)
+    private int sequence;
+
+    @Column(nullable = false)
+    private String arrivalTime;
+
+    @Column(nullable = false)
+    private String departureTime;
+
+    @Column(nullable = false)
+    private String stationId;
+}
