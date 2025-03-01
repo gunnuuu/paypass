7d705eb9a5d12a2a7c9db6cd24b490d7b1ed71e0
Kunwoo Park <105582355+gunnuuu@users.noreply.github.com>
2025-03-01 23:14:09 +0900

    이거 안된다고 빌드에서 오류남

--- src/test/java/paypass/DemoApplicationTests.java
+++ src/test/java/paypass/DemoApplicationTests.java
@@ -2,12 +2,12 @@ package paypass;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import project.paypass.PaypassApplication;
 
-@SpringBootTest
+@SpringBootTest(classes = PaypassApplication.class)
 class DemoApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
-
 }
+
