9704565e5d37f8b5b0a02918842305de07382c1b
Kunwoo Park <105582355+gunnuuu@users.noreply.github.com>
2025-03-05 01:32:16 +0900

    id 추가

--- src/main/java/project/paypass/domain/dto/LogDto.java
+++ src/main/java/project/paypass/domain/dto/LogDto.java
@@ -11,6 +11,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class LogDto {
+    private Long id;
     private String mainId;
     private LocalDateTime departureTime;
     private LocalDateTime arrivalTime;
