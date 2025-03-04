a370380f46c3a78b15b7a6be3c09640341cd6cb2
Kunwoo Park <105582355+gunnuuu@users.noreply.github.com>
2025-03-04 17:19:23 +0900

    LogDto생성

---
+++ src/main/java/project/paypass/domain/dto/LogDto.java
@@ -0,0 +1,21 @@
+package project.paypass.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import project.paypass.domain.PayCheck;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LogDto {
+    private String mainId;
+    private LocalDateTime departureTime;
+    private LocalDateTime arrivalTime;
+    private Long departureStationNumber;
+    private Long arrivalStationNumber;
+    private String routeIdList;
+    private PayCheck payCheck;
+}
