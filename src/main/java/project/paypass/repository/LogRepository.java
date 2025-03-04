d5dcf2db91d6f8b0911dcd6ee86c6ddae4e0c83f
Kunwoo Park <105582355+gunnuuu@users.noreply.github.com>
2025-03-04 17:19:44 +0900

    findByMainId함수 추가

--- src/main/java/project/paypass/repository/LogRepository.java
+++ src/main/java/project/paypass/repository/LogRepository.java
@@ -1,7 +1,10 @@
 package project.paypass.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import project.paypass.domain.Log;
+import java.util.List;
 
 public interface LogRepository extends JpaRepository<Log, Long> {
+    List<Log> findByMainId(@Param("mainId")String mainId);
 }
