a875c632c578ab15caee9c79154cf255a18a0c22
Kunwoo Park <105582355+gunnuuu@users.noreply.github.com>
2025-03-05 01:32:49 +0900

    findByLog_Main_IdAndLog_Id생성

--- src/main/java/project/paypass/repository/DetailLogRepository.java
+++ src/main/java/project/paypass/repository/DetailLogRepository.java
@@ -1,7 +1,12 @@
 package project.paypass.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import project.paypass.domain.DetailLog;
 
+import java.util.List;
+
 public interface DetailLogRepository extends JpaRepository<DetailLog, Long> {
+    @Query("SELECT dl FROM DetailLog dl JOIN FETCH dl.log l WHERE l.mainId = :mainId AND l.id = :logId")
+    List<DetailLog> findByLog_Main_IdAndLog_Id(String mainId, Long logId);
 }
