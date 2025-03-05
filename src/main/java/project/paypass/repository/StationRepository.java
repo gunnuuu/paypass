b5ede8a83a3be4cdf418c63c85b5b926e9b0bf32
Kunwoo Park <105582355+gunnuuu@users.noreply.github.com>
2025-03-05 21:23:57 +0900

    findByStationNumberIn 메소드 추가

--- src/main/java/project/paypass/repository/StationRepository.java
+++ src/main/java/project/paypass/repository/StationRepository.java
@@ -5,8 +5,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import project.paypass.domain.Station;
 
+import java.util.List;
+
 public interface StationRepository extends JpaRepository<Station, Long> {
 
     @Query("select s.busInfo from Station s where s.stationNumber = :stationNumber")
     String findBusInfoByStationNumber(@Param("stationNumber")Long stationNumber);
+
+    List<Station> findByStationNumberIn(List<Long> stationNumbers);
 }
+
