affdcc19293e9e4520dbcfc621f225ce6e7c4457
Kunwoo Park <105582355+gunnuuu@users.noreply.github.com>
2025-03-05 21:23:36 +0900

    findLocationsByStationNumbers 메소드 추가

--- src/main/java/project/paypass/service/StationService.java
+++ src/main/java/project/paypass/service/StationService.java
@@ -3,10 +3,16 @@ package project.paypass.service;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import project.paypass.domain.DetailLog;
 import project.paypass.domain.Station;
+import project.paypass.domain.dto.DetailLogDto;
+import project.paypass.domain.dto.StationDto;
 import project.paypass.repository.StationRepository;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -24,4 +30,19 @@ public class StationService {
         return stationRepository.findBusInfoByStationNumber(stationNumber);
     }
 
+    @Transactional
+    public List<StationDto> findLocationsByStationNumbers(List<Long> stationNumbers) {
+        // stationNumbers를 사용하여 Station 조회
+        List<Station> stations = stationRepository.findByStationNumberIn(stationNumbers);
+
+        // StationDto 리스트로 변환하여 반환
+        return stations.stream()
+                .map(station -> new StationDto(
+                        station.getName(),
+                        station.getStationNumber(),
+                        station.getLatitude(),
+                        station.getLongitude()
+                ))
+                .collect(Collectors.toList());
+    }
 }
