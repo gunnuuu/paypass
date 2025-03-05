e4ccf1c400746a24c7b6a4ae61e2bc67e04534a6
Kunwoo Park <105582355+gunnuuu@users.noreply.github.com>
2025-03-05 21:24:35 +0900

    /getStationLocations 매핑추가

--- src/main/java/project/paypass/controller/StationController.java
+++ src/main/java/project/paypass/controller/StationController.java
@@ -3,11 +3,15 @@ package project.paypass.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import project.paypass.domain.Station;
+import project.paypass.domain.dto.DetailLogDto;
 import project.paypass.domain.dto.StationDto;
 import project.paypass.service.StationService;
 
-import java.util.List;
+import java.util.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,7 +20,7 @@ public class StationController {
     private final StationService stationService;
 
     @GetMapping("/getStations")
-    public ResponseEntity<List<StationDto>> getStations(){
+    public ResponseEntity<List<StationDto>> getStations() {
         List<StationDto> stations = stationService.findAll().stream()
                 .map(station -> new StationDto(
                         station.getName(),
@@ -28,5 +32,15 @@ public class StationController {
         return ResponseEntity.ok(stations);
     }
 
+    @PostMapping("/getStationLocations")
+    public ResponseEntity<List<StationDto>> getStationLocations(@RequestBody Map<String, Object> request) {
+        // stationNumbers만 받음
+        List<Long> stationNumbers = (List<Long>) request.get("stationNumbers");
 
+        // stationService에서 StationDto 리스트 반환
+        List<StationDto> stationLocations = stationService.findLocationsByStationNumbers(stationNumbers);
+
+        return ResponseEntity.ok(stationLocations);
+    }
 }
+
