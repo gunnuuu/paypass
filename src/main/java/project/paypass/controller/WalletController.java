2c05c126eb4731eb71aa8a2080b307a034ca1240
Kunwoo Park <105582355+gunnuuu@users.noreply.github.com>
2025-03-13 13:55:05 +0900

    충전 출금 별로 매핑으로 처리

--- src/main/java/project/paypass/controller/WalletController.java
+++ src/main/java/project/paypass/controller/WalletController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.*;
 import project.paypass.domain.dto.WalletDto;
 import project.paypass.service.WalletHttpService;
 
+import java.util.Map;
+
 @RestController
 @RequiredArgsConstructor
 public class WalletController {
@@ -13,17 +15,27 @@ public class WalletController {
     private final WalletHttpService walletHttpService;
 
     @PostMapping("/mypage/account")
-    public ResponseEntity<Long> getAccount(@RequestParam String mainId) {
+    public ResponseEntity<Long> getAccount(@RequestBody Map<String, String> request) {
+        String mainId = request.get("email");  // JSON에서 email 가져오기
         Long accountBalance = walletHttpService.getAccount(mainId);
         return ResponseEntity.ok(accountBalance);
     }
 
-    @PostMapping("/mypage/update")
-    public ResponseEntity<Void> updateAccount(@RequestBody WalletDto walletDto){
+    @PostMapping("/mypage/plus")
+    public ResponseEntity<Void> plusAccount(@RequestBody WalletDto walletDto){
         if (walletDto.getMainId() == null) {
             throw new IllegalArgumentException("mainId가 null입니다.");
         }
-        walletHttpService.updateAccount(walletDto);
+        walletHttpService.plusAccount(walletDto);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/mypage/minus")
+    public ResponseEntity<Void> minusAccount(@RequestBody WalletDto walletDto){
+        if (walletDto.getMainId() == null) {
+            throw new IllegalArgumentException("mainId가 null입니다.");
+        }
+        walletHttpService.minusAccount(walletDto);
         return ResponseEntity.ok().build();
     }
 }
