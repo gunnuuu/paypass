19860b572c3f30367c79ac2d795ae276aac47408
Kunwoo Park <105582355+gunnuuu@users.noreply.github.com>
2025-03-13 13:55:52 +0900

    WalletRepository.java updateWalletBalance메소드 추가

--- src/main/java/project/paypass/repository/WalletRepository.java
+++ src/main/java/project/paypass/repository/WalletRepository.java
@@ -1,6 +1,9 @@
 package project.paypass.repository;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import project.paypass.domain.Wallet;
@@ -12,4 +15,8 @@ public interface WalletRepository extends JpaRepository<Wallet, Long> {
 
     List<Wallet> findByMainId(@Param("mainId")String mainId);
 
+    @Modifying
+    @Transactional
+    @Query("UPDATE Wallet w SET w.account = w.account + :amount WHERE w.mainId = :mainId")
+    int updateWalletBalance(@Param("mainId") String mainId, @Param("amount") int amount);
 }
