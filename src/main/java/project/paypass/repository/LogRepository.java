70d81fb521b417ad22ebaa8bce0237298dfac4d9
Kunwoo Park <105582355+gunnuuu@users.noreply.github.com>
2025-04-03 11:08:53 +0900

    chore(LogRepository): @repository 추가

--- src/main/java/project/paypass/repository/LogRepository.java
+++ src/main/java/project/paypass/repository/LogRepository.java
@@ -2,9 +2,12 @@ package project.paypass.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import project.paypass.domain.Log;
+
 import java.util.List;
 
+@Repository
 public interface LogRepository extends JpaRepository<Log, Long> {
     List<Log> findByMainId(@Param("mainId")String mainId);
 }
