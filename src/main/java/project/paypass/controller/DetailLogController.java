ede9e1e41e772075d2f23f5b091b70196530ded6
Kunwoo Park <105582355+gunnuuu@users.noreply.github.com>
2025-04-03 11:07:33 +0900

    feat(DetailLogController): getDetailLogs 메서드 생성

--- src/main/java/project/paypass/controller/DetailLogController.java
+++ src/main/java/project/paypass/controller/DetailLogController.java
@@ -1,14 +1,18 @@
 package project.paypass.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import project.paypass.domain.dto.DetailLogDto;
 import project.paypass.service.LogService;
 
 import java.util.List;
 import java.util.Map;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class DetailLogController {
@@ -21,12 +25,12 @@ public class DetailLogController {
         String mainId = (String) request.get("mainId");  // Flutter에서 보낸 mainId 값을 가져옴
         Long logId = Long.valueOf(request.get("logId").toString());  // Flutter에서 보낸 logId 값을 가져옴
 
-        System.out.println("mainId: " + mainId);  // mainId 출력
-        System.out.println("logId: " + logId);
         // logId에 해당하는 상세 로그를 DB에서 조회
-        List<DetailLogDto> detailLogs = logService.findByMain_IdandLog_Id(mainId,logId);
+        List<DetailLogDto> detailLogs = logService.findByMainIdAndLogId(mainId,logId);
 
 
         return ResponseEntity.ok(detailLogs);  // 200 OK 상태와 함께 상세 로그 리스트 반환
     }
+
+
 }
